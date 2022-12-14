e22790fc4c3845f6834f8e59c649e1c34f783fa3
SamuelJones24 <samdude24@outlook.com>
2022-12-14 02:42:47 -0600

    test to see if it runs on port 5000 or port 5500

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
